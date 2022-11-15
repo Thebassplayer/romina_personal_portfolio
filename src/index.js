6324c7138902e0210db4709f94c129a0bad27a12
Roy Lopez <windup.dues-0f@icloud.com>
2022-11-15 16:56:20 -0300

    Skills section finished

--- src/index.js
+++ src/index.js
@@ -9,6 +9,29 @@ import "./assets/style/style.css";
 /* Validate If Constant Exists */
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContent = document.querySelectorAll("[data-content]");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    /*Get the Node target with the data-target atribute*/
+    const target = document.querySelector(tab.dataset.target);
+
+    /*Clear all tabContents*/
+    tabContent.forEach(tabContents => {
+      tabContents.classList.remove("skills__active");
+    });
+
+    target.classList.add("skills__active");
+
+    /*Clear all tabs*/
+    tabs.forEach(tab => {
+      tab.classList.remove("skills__active");
+    });
+
+    tab.classList.add("skills__active");
+  });
+});
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
