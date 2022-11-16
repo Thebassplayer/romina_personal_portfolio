c748a9c6e02609344b3e736427985a518bab1fba
Roy Lopez <windup.dues-0f@icloud.com>
2022-11-16 12:24:53 -0300

    Modal html created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./assets/style/style.css";
+import mixitup from "mixitup";
 
 /*=============== SHOW SIDEBAR ===============*/
 
@@ -34,8 +35,23 @@ tabs.forEach(tab => {
 });
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
 
 /*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll(".work__item");
+
+function activeWork() {
+  linkWork.forEach(l => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+linkWork.forEach(l => l.addEventListener("click", activeWork));
 
 /*===== Work Popup =====*/
 
