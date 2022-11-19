2452850e24037d9bca9c99805c1c9faa6169d430
Roy Lopez <windup.dues-0f@icloud.com>
2022-11-19 10:40:51 -0300

    Footer section finished

--- src/index.js
+++ src/index.js
@@ -176,4 +176,34 @@ inputs.forEach(input => {
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+// Get all sections that have an id defined
+const sections = document.querySelectorAll("section[id]");
+
+// Event listenes for scrolling
+window.addEventListener("scroll", navHighlihter);
+
+function navHighlihter() {
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+
+  // Loop throght sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+    /* - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation Link, else remove it
+ - To know which link needs an active class, we use sectionId variable we are getting while we are looping througth sections as an selector */
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
 /*=============== SHOW SCROLL UP ===============*/
